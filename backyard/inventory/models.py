0358c6f255d329af2ac76aa3151092d3f6df40a3
Kouhei Maeda <mkouhei@palmtb.net>
2016-06-20 07:04:11 +0900

    Adds Inventories models.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- backyard/inventory/models.py
+++ backyard/inventory/models.py
@@ -1,2 +1,29 @@
+# -*- coding: utf-8 -*-
 """backyard.inventory.models."""
-# from django.db import models
+from django.db import models
+
+
+class BaseModel(models.Model):
+    """Abstract base model."""
+    id = models.AutoField(primary_key=True)
+
+    class Meta(object):
+        """meta class."""
+        abstract = True
+
+
+class Maker(BaseModel):
+    """Maker."""
+    name = models.CharField(unique=True)
+
+
+class Production(BaseModel):
+    """Productions."""
+    name = models.CharField()
+    maker = models.ForeignKey(Maker)
+
+
+class Inventory(BaseModel):
+    """Inventory."""
+    production = models.ForeignKey(Production)
+    amount = models.IntegerField()
