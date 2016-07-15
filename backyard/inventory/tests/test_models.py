cacdc3ccd4deb796f045b8388a4146305fbae6f4
Kouhei Maeda <mkouhei@palmtb.net>
2016-07-15 22:23:26 +0900

    Adds unit test Maker model.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- backyard/inventory/tests/test_models.py
+++ backyard/inventory/tests/test_models.py
@@ -1,2 +1,24 @@
 """backyard.inventory.tests.test_models"""
-# from django.test import TestCase
+from django.test import TransactionTestCase
+from django.db import IntegrityError
+from backyard.inventory.models import Maker
+
+
+class MakerTransactionTest(TransactionTestCase):
+    """transaction test of Maker."""
+
+    def test_create(self):
+        """create."""
+        maker_name = 'someone maker'
+        query = Maker(name=maker_name)
+        query.save()
+        self.assertEqual(query.__str__(), maker_name)
+
+    def test_create_fail(self):
+        """create."""
+        maker_name = 'someone maker'
+        query = Maker(name=maker_name)
+        query.save()
+        query = Maker(name=maker_name)
+        with self.assertRaises(IntegrityError):
+            query.save()
