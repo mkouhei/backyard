0f65b321941dd758472d59248c6d910afa00725d
Kouhei Maeda <mkouhei@palmtb.net>
2016-07-18 05:22:56 +0900

    Adds Product tests.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- backyard/inventory/tests/test_models.py
+++ backyard/inventory/tests/test_models.py
@@ -1,7 +1,8 @@
 """backyard.inventory.tests.test_models"""
 from django.test import TransactionTestCase
 from django.db import IntegrityError
-from backyard.inventory.models import Maker
+from backyard.inventory.models import (Maker,
+                                       Product)
 
 
 class MakerTransactionTest(TransactionTestCase):
@@ -19,5 +20,28 @@ class MakerTransactionTest(TransactionTestCase):
     def test_create_fail(self):
         """create fail."""
         query = Maker(name=self.maker_name)
+        with self.assertRaises(IntegrityError):
+            query.save()
+
+
+class ProductTransactionTest(TransactionTestCase):
+    """transaction test of Product."""
+
+    def setUp(self):
+        """initialize."""
+        self.maker = Maker(name='some maker')
+        self.maker.save()
+        self.product_name = 'some product'
+        self.query = Product(name=self.product_name, maker=self.maker)
+        self.query.save()
+
+    def test_create(self):
+        """create."""
+        self.assertEqual(self.query.__str__(), self.product_name)
+        self.assertEqual(self.query.maker, self.maker)
+
+    def test_create_fail(self):
+        """create fail."""
+        query = Product(name=self.product_name, maker=self.maker)
         with self.assertRaises(IntegrityError):
             query.save()
