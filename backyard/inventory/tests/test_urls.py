96a3473e92ea7dfe2c298cfdb11214cc311c91cd
Kouhei Maeda <mkouhei@palmtb.net>
2016-07-22 05:32:34 +0900

    Fixes admin url path.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- backyard/inventory/tests/test_urls.py
+++ backyard/inventory/tests/test_urls.py
@@ -8,7 +8,7 @@ class TestUrls(unittest.TestCase):
 
     def test_urls(self):
         """test urls."""
-        self.assertEqual(resolve('/admin/').view_name, 'admin:index')
+        self.assertEqual(resolve('/admin').view_name, 'admin:index')
         self.assertEqual(resolve('/login').view_name,
                          'backyard.inventory.views.login')
         self.assertEqual(resolve('/').view_name,
