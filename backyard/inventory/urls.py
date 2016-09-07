e8da0a4ea7b2f9e79716118fea74e843aa25e03a
Kouhei Maeda <mkouhei@palmtb.net>
2016-09-08 06:20:04 +0900

    Refines urls.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- backyard/inventory/urls.py
+++ backyard/inventory/urls.py
@@ -6,7 +6,6 @@ from django.contrib.auth.views import login, logout
 from .views.dashboard import DashboardView
 from .views.inventories import InventoriesView
 from .views.orders import OrdersView
-from .views.receives import ReceivesView
 from .views.unpacks import UnpacksView
 
 
@@ -15,11 +14,9 @@ urlpatterns = [
     url(r'^logout', logout, {'template_name': 'logout.html'}),
     url(r'^inventories(/?)(([0-9]+)?)$',
         InventoriesView.as_view()),
-    url(r'^inventories/([0-9]+)/products/([0-9]+)/orders(/?)(([0-9]+)?)$',
+    url(r'^inventories/([0-9]+)/orders(/?)(([0-9]+)?)$',
         OrdersView.as_view()),
-    url(r'^inventories/([0-9]+)/products/([0-9]+)/receives(/?)(([0-9]+)?)$',
-        ReceivesView.as_view()),
-    url(r'^inventories/([0-9]+)/products/([0-9]+)/unpacks(/?)(([0-9]+)?)$',
+    url(r'^inventories/([0-9]+)/unpacks(/?)(([0-9]+)?)$',
         UnpacksView.as_view()),
     url(r'^$', DashboardView.as_view()),
 ]
