cccbbf574f6d8dcab00af7b9a36eaa3c102a931b
Kouhei Maeda <mkouhei@palmtb.net>
2016-09-11 04:32:44 +0900

    Re-implemented Product.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- backyard/inventory/views/inventories.py
+++ backyard/inventory/views/inventories.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 """backyard.inventory.views.inventories."""
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
+from django.db.models import Q, Sum
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.utils.decorators import method_decorator
 
-from ..models.inventory import Inventory
-from ..queryset.inventory import QuantityQuerySet
+from ..models.product import Product
 
 
 class InventoriesView(TemplateView):
     """Inventories."""
-    model = Inventory
+    model = Product
 
     @method_decorator(login_required)
     def get(self, request, *args):
@@ -22,20 +21,24 @@ class InventoriesView(TemplateView):
         else:
             return self._index()
 
-    def _show(self, inventory_id):
-        inventory_obj = Inventory.objects.get(
-            Q(owner=self.request.user) &
-            Q(id=inventory_id)
-        )
-        query = QuantityQuerySet(inventory_obj)
-        return render(self.request,
-                      'inventories/show.html',
-                      {'inventory': inventory_obj,
-                       'quantity': query})
-
+    def _show(self, product_id):
+        return
+        
     def _index(self):
-        query = [(i, QuantityQuerySet(i))
-                 for i in Inventory.objects.filter(owner=self.request.user)]
+        group = self.request.user.groups.first()
+        ordered = Product.objects.filter(
+            Q(orderhistory__isnull=False) &
+            Q(orderhistory__group=group)
+        ).annotate(
+            ordered=Sum('orderhistory__ordered_quantity'),
+            received=Sum('orderhistory__received_quantity')
+        )
+        unpacked = Product.objects.extra(
+            select={'unpacked': 'quantity'}
+        ).filter(
+            Q(unpackhistory__isnull=False) &
+            Q(unpackhistory__group=group)
+        )
         return render(self.request,
                       'inventories/index.html',
-                      {'inventories': query})
+                      {'products': (ordered & unpacked).values()})
