18676a63439c1cefd8e6e62e7a78aa468a78413e
Kouhei Maeda <mkouhei@palmtb.net>
2016-09-24 05:32:43 +0900

    Adds UnpackQuerySet.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ backyard/inventory/queryset/unpacked_history.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""backyard.inventory.queryset.unpacked_history."""
+from django.db.models import Q, Sum
+
+from ..models.unpack_history import UnpackHistory
+
+
+class UnpackQuerySet(object):
+    """Unpack queries."""
+
+    def __init__(self, product_id, group):
+        """initialize."""
+        self.product_id = product_id
+        self.group = group
+        self.unpack_query = UnpackHistory.objects.filter(
+            Q(product__id=self.product_id) &
+            Q(group=self.group)
+        )
+
+    @property
+    def unpack(self):
+        return self.unpack_query.values(
+            'unpacked_at',
+            'quantity'
+        )
+
+    @property
+    def product_name(self):
+        return self.unpack_query.values('product__name').first().get('product__name')
